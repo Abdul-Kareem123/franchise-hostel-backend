c12a7616d1398c7f72c8d67543bc40692e1345be
Haripixalive <134277024+Haripixalive@users.noreply.github.com>
2024-04-03 12:32:12 +0530

    code hari commit

---
+++ src/models/contactUs.model.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+ 
+export interface contactDocument extends mongoose.Document{
+    _id?:any;
+    fullName?:string;
+    companyName?:string;
+    mobileNumber?:number;
+    emailAddress?:string;
+    pincode?:number;
+    subject?:string;
+    message?:string;
+    createdOn?:Date
+    createdBy?:string
+    modifiedOn?:Date
+    modifiedBy?:string
+}
+ 
+const contactSchema = new mongoose.Schema({
+    _id:{type:mongoose.Types.ObjectId, auto:true},
+    fullName:{type:String },
+    companyName:{type:String},
+    mobileNumber:{type:Number},
+    emailAddress:{type:String, lowercase:true, trim:true},
+    pincode:{type:Number},
+    subject:{type:String},
+    message:{type:String},
+    createdOn: {type: Date},
+    createdBy: {type: String},
+    modifiedOn: {type: Date},
+    modifiedBy: {type: String}
+   
+})
+ 
+export const contact = mongoose.model("Contact us",contactSchema)
---
+++ src/router/contactUs.routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { basicAuthUser } from "../middleware/checkAuth";
+import { checkSession } from "../utils/tokenManager";
+import { contactUs, getAllContactUs, getSingle } from "../controller/contactUs.controller";
+const router:Router=Router()
+ 
+router.post("/",
+basicAuthUser,
+// checkSession,
+contactUs
+);
+
+router.get('/',
+basicAuthUser,
+// checkSession,
+getAllContactUs
+);
+
+router.get('/singleuser',
+basicAuthUser,
+// checkSession,
+getSingle
+);
+ 
+export default router;
