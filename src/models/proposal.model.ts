6150d30b2009a2792e29af864da1f27c4036fdcb
Kaaviyan <kaaviyan@pixalive.me>
2024-07-02 14:17:07 -0800

    commit proposal model

---
+++ src/models/proposal.model.ts
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+ 
+export interface contactDocument extends mongoose.Document{
+    _id?: any;
+    name?: string;
+    phone?: number;
+    email?: string;
+    pincode?: number;
+    brandId?:any;
+    location?: string;
+    duration?: number;
+    inverstmentRange?: string;
+    isDeleted?: boolean ;
+    createdOn?: Date
+    createdBy?: string
+    modifiedOn?: Date
+    modifiedBy?: string
+}
+ 
+const contactSchema = new mongoose.Schema({
+    _id: { type: mongoose.Types.ObjectId, auto: true },
+    name: { type: String },
+    phone: { type: Number },
+    email: { type: String, lowercase: true, trim: true },
+    pincode:{ type: Number },
+    brandId:{type:mongoose.Types.ObjectId , ref:'Brand'},
+    location:{ type: String },
+    duration:{type:Number},
+    inverstmentRange: { type: String },
+    isDeleted: { type: Boolean, default: false },
+    createdOn: { type: Date },
+    createdBy: { type: String },
+    modifiedOn: { type: Date },
+    modifiedBy: { type: String }
+   
+})
+ 
+export const contact = mongoose.model("Contact us",contactSchema)
